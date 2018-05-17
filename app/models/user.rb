30ea4af04bf5747927b5b0718b089259d1c6475a
yar-ua <yar.ua.1985@gmail.com>
2018-05-17 23:56:31 +0300

    upd validation, ready to tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,18 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   # Set validations
-  validates :phone, :email, uniqueness: true
-  validates :email, :phone, :first_name, :last_name, :birth_day, presence: true, uniqueness: true
-  validates :password, length: { in: 6..20 }
-  validates :phone, numericality: { only_integer: true }
-  validates :first_name, :last_name, length: { maximum: 30 }
+  validates :phone, :email, uniqueness: {message: 'Current phone or email already exists, try another'}
+  validates :email, :phone, :first_name, :last_name, :birth_day, presence: {messaage: 'Value must be present'}
+  validates :phone, :birth_day, numericality: { only_integer: true, message: 'Value must be digit' }
+  validates :first_name, :last_name, length: { maximum: 30 , message: 'Max length must be 30 symbols'}
+  # validate, what user age 21 year or more
+  validate :user_age
+
+  
+  # check user's current age
+  def user_age
+  	errors.add(:birth_day, "Your age must be 21 years or more") if
+  	  (Date.today.year - self.birth_day.to_i) <= 21
+  end
 
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email 'asdf@asdf.com'
+    password 'qwertyuiop'
+  end
+end
