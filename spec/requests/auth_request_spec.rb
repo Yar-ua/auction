559c23f47802e2162438da72cf0a0ac4204eb3a2
yar-ua <yar.ua.1985@gmail.com>
2018-05-22 04:08:25 +0300

    auth tests finished

--- spec/requests/auth_request_spec.rb
+++ spec/requests/auth_request_spec.rb
@@ -2,6 +2,9 @@ require "rails_helper"
 
 # Resource: https://github.com/lynndylanhurley/devise_token_auth/issues/75#issuecomment-258496229
 
+# initialise constant for using in reset and change password
+RAW_TOKEN, HASHED_TOKEN = Devise.token_generator.generate(User, :reset_password_token)
+
 RSpec.describe "Auth", :type => :request do
   before do
     phone = '0672223344'
@@ -101,21 +104,27 @@ RSpec.describe "Auth", :type => :request do
       @auth_params = get_auth_params_from_login_response_headers(response)
       post user_password_path(email: @registration_params[:email], 
           redirect_url: 'auth/password/edit', headers: @auth_params)
-      @auth_params = get_auth_params_from_login_response_headers(response)
     end
 
     it 'password reset response should be 200 ok' do
       expect(response.status).to eq(200)
     end
 
-    describe 'confirmation reset passord' do
+    describe 'confirmation reset password' do
+      before do
+        reset_password
+      end
+
       it 'should be successfully' do
-        # reset password confirmation
-        user = User.last
-        get edit_user_password_path(:config => 'default', :reset_password_token => user.reset_password_token, 
-            redirect_url: 'auth/password/edit')
         expect(response.status).to eq(302)
-        #get 'http://localhost/auth/password/edit?config=default&redirect_url=auth%2Fpassword%2Fedit&reset_password_token="#{user.reset_password_token}"'
+      end
+
+      it 'password can be changed after reset' do
+        sign_in
+        @auth_params = get_auth_params_from_login_response_headers(response)
+        put user_password_path(password: '22334455', password_confirmation: '22334455'),
+            headers: @auth_params
+        expect(response.status).to eq(200)
       end
     end
   end
@@ -151,6 +160,12 @@ RSpec.describe "Auth", :type => :request do
     user = User.last
     get user_confirmation_path(:config => 'default', 
       :confirmation_token => user.confirmation_token, :redirect_url => '/')
+  end
+
+  def reset_password
+    @user = create(:user, reset_password_token: HASHED_TOKEN, reset_password_sent_at: Time.current)
+    get edit_user_password_path(:config => 'default', :reset_password_token => RAW_TOKEN, 
+        redirect_url: 'auth/password/edit')
   end
 
 end
